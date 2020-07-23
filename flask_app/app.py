6c891e9f0c173d3f91cad6b14624602082bdc362
eyvonne <eyvonne@users.noreply.github.com>
2020-07-23 09:38:16 -0700

    saving structure set up, untested

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from .models import *
 from dotenv import load_dotenv
@@ -26,10 +26,33 @@ def create_app():
     def render_insert(insert):
         return render_template('insert.html', insertion=insert)
 
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return "DB reset"
+
     @app.route('/puppy')
     def puppy():
         json = requests.get('https://dog.ceo/api/breeds/image/random').json()
         image = json['message']
         return render_template('dog.html', picture=image)
+
+    @app.route('/save_dog')
+    def save_dog():
+        json = requests.get('https://dog.ceo/api/breeds/image/random').json()
+        image = json['message']
+        breed = image
+        return render_template('save_dog.html', picture=image, breed=image)
+
+    @app.route('/saved_dog', methods=['POST'])
+    def saved_dog():
+        image = request.values['doglink']
+        breed = request.values['dogbreed']
+        name = request.values['name']
+        dog = Dog(dog=image, name=name, breed=breed)
+        DB.session.add(dog)
+        DB.session.commit()
+        return render_tempate('saved_dog.html', picture=image, breed=breed, name=name)
 
     return app
